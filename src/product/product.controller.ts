f1ce0dadfa5e876f401991992ae7167e3bb3a26f
DESKTOP-B421EOF\admin <deepak.bawa@orionesolutions.in>
2022-04-19 15:24:20 +0530

    modified the product micro service controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -10,32 +10,32 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  @MessagePattern('createProduct')
-  create(@Param() createProductDto: CreateProductDto) {
+  @EventPattern('createProduct')
+  create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
   @Get()
-  @MessagePattern('findAllProduct')
+  @MessagePattern({ cmd: 'findAllProduct' })
   findAll() {
     return this.productService.findAll();
   }
 
   @Get(':id')
-  @MessagePattern('findOneProduct')
-  findOne(@Param('id') id: ObjectId) {
+  @MessagePattern({ cmd: 'findOneProduct' })
+  findOne(@Body('id') id: ObjectId) {
     return this.productService.findOne(id);
   }
 
   @Patch(':id')
-  @MessagePattern('updateProduct')
-  update(@Param() updateProductDto: UpdateProductDto) {
+  @EventPattern('updateProduct')
+  update(@Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(updateProductDto.id, updateProductDto);
   }
 
   @Delete(':id')
-  @MessagePattern('removeProduct')
-  remove(@Param('id') id: ObjectId) {
+  @EventPattern('removeProduct')
+  remove(@Body('id') id: ObjectId) {
     return this.productService.remove(id);
   }
 }
